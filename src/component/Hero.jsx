80bcd48df4eb89430cfaf870e1158c1b3a5b1c4d
RazaNaqsh <owaisraza1704@gmail.com>
2023-08-08 14:14:45 +0530

    Add motion to hero section

--- src/component/Hero.jsx
+++ src/component/Hero.jsx
@@ -1,18 +1,39 @@
 import React from "react";
 import "../style.css/Hero.css";
 import img2 from "../images/photo1666714848-1024x576.jpeg";
+import { motion } from "framer-motion";
 
 const Hero = () => {
+  const variants = {
+    hidden: { opacity: 0, y: 220 },
+    visible: { opacity: 1, y: 0 },
+  };
   return (
     <div className="Hero">
       <div className="container">
         <div className="text-btn">
-          <div className="textup">
-          The Road To Physical Therapy Mastery: Learn The Essential Components That Make You Stand Out
-          </div>
-          <div className="chotatext">
-          It's a world which brings the most advanced and quality courses related to Physiotherapy and healthcare. Top national and international lecturers who are experts in their fields to provide you the best CPD courses at the most affordable cost.
-          </div>
+          <motion.div
+            className="textup"
+            initial="hidden"
+            animate="visible"
+            transition={{ duration: 0.8, ease: "easeOut" }}
+            variants={variants}
+          >
+            The Road To Physical Therapy Mastery: Learn The Essential Components
+            That Make You Stand Out
+          </motion.div>
+          <motion.div
+            className="chotatext"
+            initial={{ opacity: 0, y: 90 }}
+            animate="visible"
+            transition={{ duration: 0.8, ease: "easeOut" }}
+            variants={variants}
+          >
+            It's a world which brings the most advanced and quality courses
+            related to Physiotherapy and healthcare. Top national and
+            international lecturers who are experts in their fields to provide
+            you the best CPD courses at the most affordable cost.
+          </motion.div>
           <button className="btn">EXPLORE NOW</button>
         </div>
 
